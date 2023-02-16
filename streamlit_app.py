f00a8d9366870485ce616eb7e31588bbd871bb3b
Christian-Rahr <125263960+Christian-Rahr@users.noreply.github.com>
2023-02-16 10:28:50 +0100

    Update streamlit_app.py
    add header
    add text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Prents New Healthy Diner')
+
+streamlit.header('Breakfest menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
